0e959ffcdbcaf96aa755cc309f0e64205b6cfb89
TwenntyOnee <kristian.ulrych@gmail.com>
2022-04-20 18:02:09 +0200

    c

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { Post } from './post';
 
 const App = () => {
   return (
-    <Post title="TITULEK" content='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce consectetuer risus a nunc. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Mauris metus. In dapibus augue non sapien. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quisque tincidunt scelerisque libero.' date='20.4.2022'/>
+    <Post title="TITULEK" content='Lorem ipsum dolosr sit amet, consectetuer adipiscing elit. Fusce consectetuer risus a nunc. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Mauris metus. In dapibus augue non sapien. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quisque tincidunt scelerisque libero.' date='20.4.2022'/>
   );
 };
 
